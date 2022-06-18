9db4bd72e251217335880be86484b92619f51c5d
Decagon <decagon@Decagons-MacBook-Air.local>
2022-06-18 13:03:21 +0100

    save

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,24 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+
+  function isValid(str) {
+    let obj = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+      },
+      stack = [];
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "(" || str[i] === "{" || str[i] === "[") {
+        stack.push(str[i]);
+      } else if (str[i] !== obj[stack.pop()]) {
+        return 'invalid';
+      }
+    }
+    return stack.length > 0 ? 'invalid' : 'valid';
+  }
+
+  console.log(isValid("()[]{}"));
 
 module.exports = isValid;
