4209695b9e6652337c61b2f5a771c51e4a28bdd1
Decagon <decagon@Decagons-MacBook-Air.local>
2022-06-18 13:08:26 +0100

    saves

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,25 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+  function transpose(array) {
+    let rows = array.length;
+    let columns = array[0].length;
+    let number = [];
 
-module.exports = transpose;
+    for(let i = 0; i < columns; i++){
+      number[i] = Array(rows);
+    }
+
+    for (let j = 0; j < rows; j++){
+      for(let k = 0; k < columns; k++){
+        number[k][j] = array[j][k];
+      }
+    }
+
+
+    return number;
+
+  }
+
+  module.exports = transpose
+
